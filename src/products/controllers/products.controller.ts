e47ebe2ff85ff4abf68a341507e5f878802b2b0a
Saymon Damásio <saymon.damasio95@gmail.com>
2022-02-18 11:33:35 -0300

    refactor: test expose url photos product

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -15,6 +15,7 @@ import { Roles } from 'src/auth/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Role } from 'src/users/enums/role.enum';
+import { Product } from '../entities/product.entity';
 import { CreateProductService } from '../services/create-product.service';
 import { FindProductService } from '../services/find-product.service';
 import { ListProductsService } from '../services/list-products.service';
@@ -56,29 +57,31 @@ export class ProductsController {
 
   @Get()
   index() {
-    return [
-      {
-        id: '83267366-a729-47b2-ad07-6abaf9c0329e',
-        sku: 'AB1234',
-        title: 'Produto 1',
-        availability: true,
-        description: 'Descrição do Produto 1',
-        height: '25.00',
-        width: '20.00',
-        length: '50.00',
-        weight: '250.00',
-        stock: 10,
-        photos: [
-          '76e5ddbe9a259ff546bd-paul-gaudriault-a-QH9MAAVNI-unsplash.jpg',
-          '744990746c93876787e6-revolt-164_6wVEHfI-unsplash.jpg',
-        ],
-        price: '25000.00',
-        promotional_price: '20000.00',
-        category_id: '4820d1d5-e38c-479d-81aa-d6298bfe324f',
-        created_at: '2022-02-18T14:59:56.969Z',
-        updated_at: '2022-02-18T15:53:00.546Z',
-      },
-    ];
+    const product = new Product();
+
+    Object.assign(product, {
+      id: '83267366-a729-47b2-ad07-6abaf9c0329e',
+      sku: 'AB1234',
+      title: 'Produto 1',
+      availability: true,
+      description: 'Descrição do Produto 1',
+      height: '25.00',
+      width: '20.00',
+      length: '50.00',
+      weight: '250.00',
+      stock: 10,
+      photos: [
+        '76e5ddbe9a259ff546bd-paul-gaudriault-a-QH9MAAVNI-unsplash.jpg',
+        '744990746c93876787e6-revolt-164_6wVEHfI-unsplash.jpg',
+      ],
+      price: '25000.00',
+      promotional_price: '20000.00',
+      category_id: '4820d1d5-e38c-479d-81aa-d6298bfe324f',
+      created_at: '2022-02-18T14:59:56.969Z',
+      updated_at: '2022-02-18T15:53:00.546Z',
+    });
+
+    return [product];
   }
 
   @Get(':id/stock')
